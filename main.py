eca2cd1297b408851dbbe401b96124023959c14e
RyannWilcox <ryanwilcox1@yahoo.com>
2015-10-15 18:43:51 -0400

    work in progress

---
+++ main.py
@@ -0,0 +1,24 @@
+import finsymbols
+
+from pprint import pprint
+
+# functions:
+#   get_amex_symbols()
+#   get_nyse_symbols()
+#   get_nasdaq_symbols()
+#   get_sp500_symbols()
+
+nyse = finsymbols.get_nyse_symbols()
+count = 0
+for num in nyse:
+    count += 1
+    pprint(num)
+    
+
+#print count
+#def main():
+#    print "main stock loop goes in here............"
+
+
+#if __name__ == '__main__':
+#    main()
---
+++ stockformula.py
@@ -0,0 +1,140 @@
+from yahoo_finance import Share
+from pprint import pprint
+
+import finsymbols
+import time
+import datetime
+import csv
+
+# creates a list of stocks from a provided csv file
+def createListOfStocks():
+    listOfStockNames = []
+    initialAmex = finsymbols.get_amex_symbols()
+    initialNasdaq = finsymbols.get_nasdaq_symbols()
+    initialNyse = finsymbols.get_nyse_symbols()
+    
+    nyse = [stockInfo['symbol'].strip() for stockInfo in initialNyse]
+    nasdaq = [stockInfo['symbol'].strip() for stockInfo in initialNasdaq]
+    amex = [stockInfo['symbol'].strip() for stockInfo in initialAmex]
+    
+    return nyse + nasdaq + amex
+
+
+# Average Volume over 20 days...
+def getAverageVolumeOver20Days(stockName):
+    stock = Share(stockName)
+    currentDay = datetime.date.today()    
+    difference = datetime.timedelta(days=100)
+    day = currentDay - difference
+    
+    stockData = stock.get_historical(str(day),str(currentDay))
+    
+    twentyDayVolAvg = 0
+    count = 1
+
+    if stockData == []:
+        return 0       
+    for date in stockData:
+        if count <= 20:
+            twentyDayVolAvg = twentyDayVolAvg + float(stockData[0]['Volume'])
+            count += 1
+        elif count > 20: 
+            # count is greater than 20 days so we want to exit the loop
+            break
+            
+    return twentyDayVolAvg / 20
+
+ 
+def findLastOrPLow(stock, curDay):
+     data = ()
+     dayCount = 1
+     check = False
+     
+     while check == False:
+         difference = datetime.timedelta(days = dayCount)
+         day = curDay[1] - difference
+         stockInfo = stock.get_historical(str(day), str(day))
+         
+         if stockInfo != []:
+             data = float(stockInfo[0]['Low']), day
+             check = True
+         else:
+            dayCount += 1
+            
+     return data
+
+
+def findP2ToP4Close(stock, curDay):
+    data = ()
+    dayCount = 1
+    check = False
+    
+    while check == False:
+        difference = datetime.timedelta(days = dayCount)
+        day = curDay[1] - difference
+        stockInfo = stock.get_historical(str(day), str(day))
+        
+        if stockInfo != []:
+            data = float(stockInfo[0]['Close']), day
+            check = True
+        else:
+           dayCount += 1
+           
+    return data
+#****************************************************    
+  
+#****************************************************
+# Last = This is the lowest price the stock traded at that day
+# PLow = Previous day's low
+# P1Close = Previous day's close
+# P2Close = The close the day before P1
+# P3Close = The close the day before P2
+# P4Close = The close the day before P3
+#
+# ---------------------------------------------------
+# Formula
+# ---------------------------------------------------
+# VolAvg20 > 350,000
+# Last     > PLow
+# P1Close  < P2Close
+# P2Close  < P3Close
+# P3Close  < P4Close
+######################################################
+def pullBackSwingTradeFormula(stockName):
+    stock = Share(stockName)
+    currentDay = datetime.date.today()
+    
+    #default tuple
+    theDay = 0.0 , currentDay
+    
+    Last = findLastOrPLow(stock,theDay)
+    PLow = findLastOrPLow(stock, Last)
+    
+    # P1Close is a special case because it
+    # is on the same day as PLow
+    stockInfo = stock.get_historical(str(PLow[1]), str(PLow[1]))
+    P1Close = float(stockInfo[0]['Close']), PLow[1]
+    
+    P2Close = findP2ToP4Close(stock, P1Close)
+    P3Close = findP2ToP4Close(stock, P2Close)
+    P4Close = findP2ToP4Close(stock, P3Close)
+    
+    if (Last[0] > PLow[0]) and (P1Close[0] < P2Close[0]) and (P2Close[0] < P3Close[0]) and (P3Close[0] < P4Close[0]):
+        print  "\(^_^)/ Made it through the formula  " ,stockName
+    else:
+        print stockName, " does not pass the formula  \(*_*)/"
+
+
+
+#***************************************************************
+# combine all stock lists for now.
+# I might make it possible to choose what list to
+# use instead of all 3
+largeStockList = createListOfStocks()
+
+for stock in largeStockList:
+    avg = getAverageVolumeOver20Days(stock)
+    if avg > 350000:
+       pullBackSwingTradeFormula(stock)
+    else:
+        print stock, " does not pass the formula  \(*_*)/"
