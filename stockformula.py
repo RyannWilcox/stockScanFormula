43d2104a11bd8d342d2569813cd3f91b1e345e26
RyannWilcox <ryanwilcox1@yahoo.com>
2015-10-24 12:25:44 -0400

    final list now saved to txt file

--- stockformula.py
+++ stockformula.py
@@ -89,7 +89,6 @@ def findP1Close(stock,curDay):
 def pullBackSwingTradeFormula(stockName):
     stock = Share(stockName)
     currentDay = datetime.date.today()
-    
     #default tuple
     theDay = 0.0 , currentDay
     
@@ -98,6 +97,8 @@ def pullBackSwingTradeFormula(stockName):
     
     # P1Close is a special case because it
     # is on the same day as PLow
+    # Had to add an if stmt in here because yahoo_finance
+    # would randomly give blank data sets for no reason..
     stockInfo = stock.get_historical(str(PLow[1]), str(PLow[1]))
     if stockInfo == []:
         P1Close = 0.0, PLow[1]
@@ -111,15 +112,31 @@ def pullBackSwingTradeFormula(stockName):
     
     if (Last[0] > PLow[0]) and (P1Close[0] < P2Close[0]) and (P2Close[0] < P3Close[0]) and (P3Close[0] < P4Close[0]):
         print  "\(^_^)/ Made it through the formula  " ,stockName
+        return stockName
+    else:
+        return ''
 
-
-
+# saves the list of stocks that passed the formula
+# This function appends to a text file so the
+# previous list of stocks will remain.
+def saveToFile(stockList):
+    currentDay = datetime.date.today()
+    file = open("final_stock_list","a")
+    file.write('\n')
+    file.write( "Stock formula run on -> "+ str(currentDay) + '\n')
+    
+    for item in stockList:
+        file.write(item + '\n')
+    file.write('****************************************** \(^_^)/')
+    file.write('\(^_^)/***********************************')
+    
 #***************************************************************
 # combine all stock lists for now.
 # I might make it possible to choose what list to
 # use instead of all 3
 largeStockList = createListOfStocks()
 
+finalStockList = []
 # Use a random stock as a way to find the last 20 trading days
 # This makes it so we only have to get these dates one time, as
 # opposed to finding them every time we need to get the average.
@@ -128,5 +145,11 @@ last20 = getLast20TradingDays('AAPL')
 for stock in largeStockList:
     avg = getAverageVolumeOver20Days(stock, last20[0],last20[1])
     if avg > 350000:
-        pullBackSwingTradeFormula(stock)
+        stockName = pullBackSwingTradeFormula(stock)
+        
+        if stockName != '':
+            finalStockList.append(stockName)
+        
+
+saveToFile(finalStockList)
 print "ITS FINISHED!"
